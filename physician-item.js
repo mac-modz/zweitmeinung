370e9aea62fd22419b4e4dc90a14fec5483b1033
mac-modz <61435487+mac-modz@users.noreply.github.com>
2022-12-20 13:36:27 +0100

    image wip

--- physician-item.js
+++ physician-item.js
@@ -109,10 +109,10 @@ window.addEventListener('DOMContentLoaded', function() {
 			card.querySelector('#physician-name').innerText = doc.name;
 			card.querySelector('#physician-fee').innerText = doc.price.amount + '/h';
 			card.querySelector('#physician-specialty').innerText = doc.categories[0].name;
-			
-			let imgDoc = document.createElement("img")
-			imgDoc.src = doc.managing_user.practitioner.image
-			card.querySelector('.image-cover').appendChild(imgDoc);
+			console.log('>>>',  doc, doc.managing_user.practitioner.image)
+			//let imgDoc = document.createElement("img")
+			//imgDoc.src = doc.managing_user.practitioner.image
+			card.querySelector('.image-cover').src = doc.managing_user.practitioner.image;
 			doc.slots =
 				_.chain(doc.slots)
 				.map(slot => {
